ee64fd729f8542c3d9fa2d40ba442e0dc7a21814
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2025-01-02 17:46:51 +0330

    Update setup-client.sh

--- setup-client.sh
+++ setup-client.sh
@@ -1,5 +1,12 @@
 # Client side installation and mount (uncomment and edit server-ip as needed)
-sudo apt install -y nfs-common
+if [ -x "$(command -v apt)" ]; then
+    sudo apt install -y nfs-common
+elif [ -x "$(command -v yum)" ]; then
+    sudo yum install -y nfs-utils
+else
+    echo "Package manager not supported. Exiting."
+    exit 1
+fi
 
 sudo mount server-ip:/mnt/shared_nfs /mnt
 
